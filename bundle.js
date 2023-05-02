b3754b98c4ec5717d343166b540eeb53c8b11a94
Lluis Peinado <lpeinado@stelladot.com>
2023-05-02 22:45:53 +0200

    main: adding plugins to rollup for json and minify output

--- bundle.js
+++ bundle.js
@@ -1,8 +1,9 @@
-var foo = 'hello world!';
+var version = "1.0.0";
 
 // src/main.js
+
 function main () {
-	console.log(foo);
+	console.log('version' + version);
 }
 
 export { main as default };
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,16 @@
+import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
+
 export default {
 	input: 'src/main.js',
-	output: {
+	output: [{
 		file: 'bundle.js',
 		format: 'es'
-	}
+	},{
+		file:'bundle.min.js',
+		format:'iife',
+		name:'version',
+		plugins: [terser()]
+	}],
+	plugins:[json()]
 };
